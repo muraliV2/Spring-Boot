2e4aab3fae4c425b9b39006638585c5bbdd5f105
Murali <muralisurya2004@gmail.com>
2026-02-05 23:41:15 +0530

    Create UserRepository.java

---
+++ project1/UserRepository.java
@@ -0,0 +1,13 @@
+ package com.example.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.entity.User;
+
+public interface UserRepository 
+{
+
+	List<User> user = new ArrayList<User>();
+
+}
