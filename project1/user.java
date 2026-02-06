a5e77065410de95462fd8c2486c2c8a6139a994d
Murali <muralisurya2004@gmail.com>
2026-02-06 23:49:01 +0530

    Update user.java

--- project1/user.java
+++ project1/user.java
@@ -1,11 +1,47 @@
 package com.example.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User 
 {
+	@Id
 private int UserId;
 private String UserName;
 private String Email;
 private String Password;
 private long Contact;
+public int getUserId() {
+	return UserId;
+}
+public void setUserId(int userId) {
+	UserId = userId;
+}
+public String getUserName() {
+	return UserName;
+}
+public void setUserName(String userName) {
+	UserName = userName;
+}
+public String getEmail() {
+	return Email;
+}
+public void setEmail(String email) {
+	Email = email;
+}
+public String getPassword() {
+	return Password;
+}
+public void setPassword(String password) {
+	Password = password;
+}
+public long getContact() {
+	return Contact;
+}
+public void setContact(long contact) {
+	Contact = contact;
+}
+
 
 }
