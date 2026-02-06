8336130f141fa35ee02860b8cde879e41531ed29
Murali <muralisurya2004@gmail.com>
2026-02-06 23:49:17 +0530

    Update mainclass.java

--- project1/mainclass.java
+++ project1/mainclass.java
@@ -1,13 +1,19 @@
 package com.example.userapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserappApplication.class, args);
+    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
