185737554d0a86e2994ed19efb0d0499ff5913e7
Murali <muralisurya2004@gmail.com>
2026-02-07 00:31:33 +0530

    Create userdao.java

---
+++ project1/userdao.java
@@ -0,0 +1,18 @@
+package com.example.userdao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.User;
+import com.example.repository.UserRepository;
+
+@Repository
+public class Userdao {
+
+    @Autowired
+    private UserRepository userrepository;
+
+    public User registerUser(User user) {   // ✅ fixed name
+        return userrepository.save(user);
+    }
+}
