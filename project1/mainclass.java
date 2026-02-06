f16090928c61d6e3253872a9d2ee3a50a0ca4281
Murali <muralisurya2004@gmail.com>
2026-02-07 00:31:09 +0530

    Update mainclass.java

--- project1/mainclass.java
+++ project1/mainclass.java
@@ -3,9 +3,15 @@ package com.example.userapp;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example")
+@EnableJpaRepositories(basePackages = "com.example.repository")
+@EntityScan(basePackages = "com.example.entity")
 public class UserappApplication {
 
     public static void main(String[] args) {
