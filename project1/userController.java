7f4587bd2f16ae76222c409ab6fba3983ad383ba
Murali <muralisurya2004@gmail.com>
2026-02-08 00:31:19 +0530

    Update userController.java

--- project1/userController.java
+++ project1/userController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.entity.User;
 import com.example.service.UserService;
 import com.example.userdto.Userdto;
 
@@ -20,6 +23,13 @@ public class UserController {
     @PostMapping("/register")
     public Userdto registerUser(@RequestBody Userdto dto) {
         return userService.registerUser(dto);
+    }
+    
+    @GetMapping("/getuser")
+    public List<User>  userdetails()
+    {
+    	return userService.userdetails();
+		
     }
     
 //    @GetMapping("/test")
