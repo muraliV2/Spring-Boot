fde6271fda09e585cf6a1512cc8d0538a002a10b
Murali <muralisurya2004@gmail.com>
2026-02-10 23:53:51 +0530

    Update userController.java

--- project1/userController.java
+++ project1/userController.java
@@ -1,40 +1,55 @@
-package com.example.controller;
+package com.example.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.catalina.mapper.Mapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import com.example.entity.User;
-import com.example.service.UserService;
+import com.example.repository.UserRepository;
+import com.example.userdao.Userdao;
 import com.example.userdto.Userdto;
 
-@RestController
-@RequestMapping("/user")
-public class UserController {
+@Service
+public class UserService {
+	
+	
+	
 
     @Autowired
-    private UserService userService;
+    private Userdao dao;
 
-    @PostMapping("/register")
-    public Userdto registerUser(@RequestBody Userdto dto) {
-        return userService.registerUser(dto);
+    @Autowired
+    private ModelMapper mapper;
+
+    public Userdto registerUser(Userdto dto) {
+
+        User user = mapper.map(dto, User.class);
+        User savedUser = dao.registerUser(user);
+        return mapper.map(savedUser, Userdto.class);
     }
-    
-    @GetMapping("/getuser")
-    public List<User>  userdetails()
-    {
-    	return userService.userdetails();
+
+	public List<User> userdetails() {
+	return 	dao.userdetails();
 		
+	}
+
+	public Userdto getuserbyId(int userId) {
+        Optional<User> opt = dao.getuserbyId(userId);
+
+        if (opt.isPresent()) {
+            return mapper.map(opt.get(), Userdto.class);
+        } else {
+            throw new RuntimeException("User not found with id " + userId);
+        }
     }
-    
-//    @GetMapping("/test")
-//    public String test() {
-//        return "Working";
-//    }
+
+	
+	
+	
+	
 
 }
