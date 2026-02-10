9fdcbe2345a114fde3808b65a6c475b7e24f4a21
Murali <muralisurya2004@gmail.com>
2026-02-10 23:55:44 +0530

    Update userdao.java

--- project1/userdao.java
+++ project1/userdao.java
@@ -1,12 +1,14 @@
 package com.example.userdao;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.User;
 import com.example.repository.UserRepository;
+import com.example.userdto.Userdto;
 
 @Repository
 public class Userdao {
@@ -14,13 +16,22 @@ public class Userdao {
     @Autowired
     private UserRepository userrepository;
 
-    public User registerUser(User user) {   // ✅ fixed name
+    public User registerUser(User user) 
+    {  
         return userrepository.save(user);
     }
 
 	public List<User> userdetails() 
 	{
 	return userrepository.findAll();
+	
 		
+	}
+	
+	public Optional<User> getuserbyId(int userId)
+	{
+		
+		 Optional<User> opt  = userrepository.findById(userId);
+		 return opt;
 	}
 }
