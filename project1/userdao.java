61ff868a306069e8bacf27d61036b5ca7e9178e3
Murali <muralisurya2004@gmail.com>
2026-02-13 01:25:11 +0530

    Update userdao.java

--- project1/userdao.java
+++ project1/userdao.java
@@ -18,7 +18,19 @@ public class Userdao {
 
     public User registerUser(User user) 
     {  
-        return userrepository.save(user);
+    	
+//    	Optional<User> opt = userrepository.findById(user.getUserId());
+//    	if(opt.isPresent())
+//    	{
+//    		throw new IllegalArgumentException("already value present in thsi Id");
+//    	}
+//    	else
+//    	{
+//    		return userrepository.save(user);
+//    		
+//    	}
+    	return userrepository.save(user);
+       
     }
 
 	public List<User> userdetails() 
@@ -36,5 +48,39 @@ public class Userdao {
 
 	public User getuserbyemail(String email) {
 		return userrepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("Email doesnt Exist"));
+	}
+
+	public  User updateuser(User user1) 
+	{
+	Optional<User> opt = userrepository.findById(user1.getUserId());
+	if(opt.isPresent())
+	{
+		return userrepository.save(user1);
+		
+		
+	}
+	else
+	{
+		throw new IllegalArgumentException("There is a problem in this code");
+	}
+	
+	}
+
+	public String deleteuser(int userId) {
+		
+		Optional<User> opt = userrepository.findById(userId);
+		if(opt.isPresent())
+		{
+			 userrepository.deleteById(userId);
+			 return "User Deleted Successfully";
+			
+		}
+		else
+		{
+			
+			throw new IllegalArgumentException("there is  a error in thsi program");
+		}
+		
+		
 	}
 }
