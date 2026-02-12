8533e1d6e5cdd620a49aa6444a3ee2c77ba06598
Murali <muralisurya2004@gmail.com>
2026-02-13 01:26:00 +0530

    Create responsestructure.java

---
+++ project1/responsestructure.java
@@ -0,0 +1,17 @@
+package com.example.util;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class ResponseStructure<T>
+{
+	private T data;
+	private LocalDateTime timeStamp;
+	private String message;
+	private int StatusCode;
+	
+	
+
+}
