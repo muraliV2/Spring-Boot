a8c1a928a0936581c031d8537dc1ef80f52cb171
Murali <muralisurya2004@gmail.com>
2026-03-26 23:26:31 +0530

    Update userController.java

--- project1/userController.java
+++ project1/userController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
 import com.example.entity.User;
 import com.example.service.UserService;
 import com.example.userdto.Userdto;
