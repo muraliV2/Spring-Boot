3f5feed419d356bf6fc35ce71f7377c5e2ecc929
Murali <muralisurya2004@gmail.com>
2026-03-28 22:34:36 +0530

    Update userdao.java

--- project1/userdao.java
+++ project1/userdao.java
@@ -3,7 +3,6 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import com.example.entity.User;
 import com.example.repository.UserRepository;
 import com.example.userdto.Userdto;
