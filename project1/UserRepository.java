fde8a9bce0562c53f24b6a3da81e8f200c5a2ac7
Murali <muralisurya2004@gmail.com>
2026-03-30 23:19:51 +0530

    Update UserRepository.java

--- project1/UserRepository.java
+++ project1/UserRepository.java
@@ -1,11 +1,9 @@
 package com.example.repository;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.entity.User;
 import com.example.userdto.Userdto;
 public interface UserRepository extends JpaRepository<User, Integer> {
-
 	Optional<User> findByEmail(String email);
 
 }
