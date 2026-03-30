7f812803e18901fce7e8119be41cbc4051eb6a91
Murali <muralisurya2004@gmail.com>
2026-03-30 23:19:42 +0530

    Update UserRepository.java

--- project1/UserRepository.java
+++ project1/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.entity.User;
 import com.example.userdto.Userdto;
 public interface UserRepository extends JpaRepository<User, Integer> {
